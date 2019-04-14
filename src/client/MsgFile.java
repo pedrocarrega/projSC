3aacd5bd3f028c7ed550056155bf041e3d90e4d9
Pedro Carrega <pedro.carrega@outlook.com>
2019-04-14 13:59:07 +0100

    implementing packages & starting UserManager

--- src/client/MsgFile.java
+++ src/client/MsgFile.java
@@ -1,3 +1,4 @@
+package client;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
---
+++ src/server/UserManager.java
@@ -0,0 +1,26 @@
+package server;
+import java.util.Scanner;
+
+public class UserManager {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String user, pass;
+		
+		//apresentar opcoes
+		System.out.println(presentOptions());
+		//input
+		String[] input = sc.nextLine().split(" ");
+		//prob switch
+		
+		
+		
+	}
+
+	private static String presentOptions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

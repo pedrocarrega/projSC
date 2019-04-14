23d803cac7771cac91a31f936661998e47ccdfe8
Pedro Carrega <pedro.carrega@outlook.com>
2019-04-14 17:55:08 +0100

    implementing the menu and options

--- src/server/UserManager.java
+++ src/server/UserManager.java
@@ -6,21 +6,56 @@ public class UserManager {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		String user, pass;
-		
-		//apresentar opcoes
-		System.out.println(presentOptions());
-		//input
-		String[] input = sc.nextLine().split(" ");
-		//prob switch
-		
-		
-		
+
+		while(true) {
+
+			//apresentar opcoes
+			System.out.println(presentOptions());
+			//input
+			String[] input = sc.nextLine().split(" ");
+
+			switch (input[0]) {
+			
+			case "add":
+				
+				
+
+				break;
+
+			case "edit":
+				break;
+
+			case "remove":
+				break;
+
+			case "quit":
+				sc.close();
+				System.exit(0); //fecha o programa
+
+			default:
+				System.out.println("Comando invalido, por favor volte a inserir o comando\n\n\n");
+
+				break;
+			}
+		}
+
+
+
 	}
 
 	private static String presentOptions() {
-		// TODO Auto-generated method stub
-		return null;
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("-----------------------------------------------------------------------------\n");
+		sb.append("add <username> <password> - adiciona conta de utilizador ao servidor\n");
+		sb.append("edit <username> <old password> <new password> - altera a password do utilizador\n");
+		sb.append("remove <username> <password> - remove conta de utilizador do servidor\n");
+		sb.append("quit - sai do programa\n");
+		sb.append("-----------------------------------------------------------------------------\n");
+
+		return sb.toString();
 	}
+	
 
 }

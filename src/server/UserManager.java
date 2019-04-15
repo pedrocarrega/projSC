36ca91351a4b803ed4c1a66cc0a91596407b10ab
Pedro Carrega <pedro.carrega@outlook.com>
2019-04-15 22:45:32 +0100

    merging vasco changes

--- src/server/UserManager.java
+++ src/server/UserManager.java
@@ -1,5 +1,24 @@
 package server;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Random;
 import java.util.Scanner;
+
+import javax.crypto.Mac;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 
 public class UserManager {
 
@@ -17,13 +36,19 @@ public class UserManager {
 			switch (input[0]) {
 			
 			case "add":
-				
-				
-
+				if(addUser(input[1] , input[2], args[0])) {
+					System.out.println("User adicionado com sucesso");
+				}else {
+					System.out.println("Username já está em uso");
+				}
 				break;
 
 			case "edit":
-				break;
+				if(editUser(input[1], input[2], input[3], args[0])) {
+					System.out.println("Password atualizada com sucesso");
+				}else {
+					System.out.println("Dados atuais incorretos, tente novamente");
+				}
 
 			case "remove":
 				break;
@@ -57,5 +82,141 @@ public class UserManager {
 		return sb.toString();
 	}
 	
-
+	/**
+	 * Devolve uma linha no formato proposto mas com a palavra pass depois do hash já com salt
+	 * @param userData formato = username:password
+	 * @throws NoSuchAlgorithmException 
+	 */
+	private static String hashingDados(String userData) throws NoSuchAlgorithmException {
+		Random rnd = new Random();
+		int salt = rnd.nextInt();
+		String[] splitted = userData.split(":");
+		String nPW = salt + splitted[2];
+		MessageDigest md = MessageDigest.getInstance("SHA");
+		byte[] hashed = md.digest(nPW.getBytes());
+		String pwHashed = new String(hashed);
+		return splitted[0] + ":" + salt + ":" + pwHashed;
+	}
+	
+	private static boolean addUser(String username, String password, String managerPW) {
+		File f = new File("users.txt");
+		
+		try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter(f.getName()));
+			BufferedReader br = new BufferedReader(new FileReader(f.getName()));
+			String linha;
+			
+			while((linha = br.readLine()) != null) {
+				String[] lineSplitted = linha.split(":");
+				if(lineSplitted[0].equals(username)) {
+					bw.close();
+					br.close();
+					return false;
+				}
+			}
+			bw.write(hashingDados(username + ":" + password) + "\n");
+			bw.close();
+			br.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		geraMAC(managerPW);
+		return true;
+	}
+	
+	private static boolean editUser(String username, String oldPW, String newPW, String managerPW) {
+		File f = new File("users.txt");
+		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(f.getName()));
+			String linha;
+			while((linha = br.readLine()) != null) {
+				String[] lineSplitted = linha.split(":");
+				if(lineSplitted[0].equals(username)) {
+					String dadosHashed = hashingDados(username + ":" + oldPW);
+					if(dadosHashed.equals(linha)) {
+						File tempFile = new File("usersTemp.txt");
+						BufferedWriter bw = new BufferedWriter(new FileWriter(tempFile.getName()));
+						//String currentLine;
+						br.reset();
+						while((linha = br.readLine()) != null) {
+							if(!linha.equals(username)) {
+								bw.write(linha);
+							}
+						}
+						bw.close();
+						br.close();
+						geraMAC(managerPW);
+						return true;
+					}
+				}
+			}
+			br.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+		
+	}
+	
+	private static void geraMAC(String managerPW) {
+		byte[] salt = {(byte) 0xc9, (byte) 0x36, (byte) 0x78, (byte) 0x99, (byte) 0x52, 
+				(byte) 0x3e, (byte) 0xea, (byte) 0xf2};
+		PBEKeySpec keySpec	= 	new	PBEKeySpec(managerPW.toCharArray(), salt, 20);  
+		SecretKeyFactory kf;
+		SecretKey key;
+		Mac mac;
+		File f = new File("users.txt");
+		try {
+			mac	= Mac.getInstance("HmacSHA1");
+			kf = SecretKeyFactory.getInstance("PBEWithHmacSHA256AndAES_128");
+			key = kf.generateSecret(keySpec);
+			mac.init(key);
+			
+			BufferedReader br = new BufferedReader(new FileReader(f.getName()));
+			String linha;
+			while((linha = br.readLine()) != null) {
+				mac.update(linha.getBytes());
+			}
+			br.close();
+			FileOutputStream fos = new FileOutputStream("mac1.txt");
+			ObjectOutputStream	oos	= new ObjectOutputStream(fos);
+			oos.write(mac.doFinal());
+			oos.close();
+			fos.close();
+			File file = new File("mac1.txt");
+			f.delete();
+			file.renameTo(new File("mac.txt"));
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeySpecException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }

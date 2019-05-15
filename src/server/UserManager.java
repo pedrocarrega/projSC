828ea4366b2bb683daf67869fa287191bbafa09d
Pedro Carrega <pedro.carrega@outlook.com>
2019-05-15 23:27:27 +0100

    warnings

--- src/server/UserManager.java
+++ src/server/UserManager.java
@@ -8,7 +8,6 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
